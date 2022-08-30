5cdb932e6cc4687771718219c13ef2df4def3e8f
saber-jf <saber.jafari.rendi@gmail.com>
2022-08-30 19:27:38 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1250px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+
+    extend: {
+      colors: {
+        body: "#191A1F",
+        primary: "#D22F27",
+        secondary: "#2D2D35",
+        lightColor: "#696972",
+        lightGray: "#9696A3",
+        yellow: "#F9D729",
+        white: "#fff",
+        borderColor: "#707070",
+      },
+    },
+  },
+  plugins: [],
+};
